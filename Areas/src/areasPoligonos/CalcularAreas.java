1168e159c2f659880791de37fe76eeee9c973e21
talve <talve@DARIUS-PC.home>
2025-03-21 09:29:35 +0100

    /Empezaremos por el área de un triángulo, declarando las variables base y altura

--- Areas/src/areasPoligonos/CalcularAreas.java
+++ Areas/src/areasPoligonos/CalcularAreas.java
@@ -4,7 +4,10 @@ public class CalcularAreas {
 
 	public static void main(String[] args) {
 		// Este programa calculará las áreas de polígonos regulares
-
+		 
+		//Empezaremos por el área de un triángulo, declarando las variables base y altura
+		int base, altura;
+		
 	}
 
 }
